dbca395389cb2ebb58fcdf354aa80722d01e0622
Dan Mercer <danielmercer1@gmail.com>
2018-01-02 23:03:03 -0700

    Added tests for set/setGlobal/get

--- test/test.js
+++ test/test.js
@@ -2,11 +2,155 @@
 var assert = require('assert');
 var path = require('path');
 var fs = require('fs');
+var mock = require('mock-fs');
 var applicationConfigPath = require('application-config-path');
 
 var Config = require('../src/index.js');
 
 const APP_NAME = 'gitlike-config-test';
+const globalConfigDir = applicationConfigPath(APP_NAME);
+const globalConfigPath = path.join(globalConfigDir, "config.json");
+const localConfigName = `.${APP_NAME}.config.json`;
+const localConfigPath = path.join(".", localConfigName);
+
+function assertFileContains(path, jsonContents) {
+	assert.deepEqual(JSON.parse(fs.readFileSync(path)), jsonContents);
+}
+
+const dummyLocalConfig = {
+	test: "simple",
+	also: {
+		test: {
+			a: "more complex path"
+		}
+	},
+	override: "local"
+};
+const dummyGlobalConfig = {
+	testGlobal: "bagel",
+	override: "global",
+	override2: "global"
+};
+const dummyDefaultConfig = {
+	override: "default",
+	override2: "default",
+	testDefault: "potato",
+};
+
+var conf = new Config({
+	name: APP_NAME,
+	autoLoad: false,
+	defaults: dummyDefaultConfig,
+});
+
+describe("set (local)", () => {
+
+	beforeEach(() => {
+		mock({
+			// Nothing (the working directory exists by default)
+		});
+		conf.load();
+	});
+	afterEach(() => {
+		mock.restore();
+	});
+
+	it("should set a basic property", () => {
+		conf.set("test", 1);
+		assertFileContains(localConfigPath, {
+			test: 1
+		})
+	})
+
+	it("should work with nested paths", () => {
+		conf.set("test.some.path", 1);
+		assertFileContains(localConfigPath, {
+			test: { some: { path: 1 } }
+		})
+	})
+
+	it("should work with arrays", () => {
+		conf.set(["test","some","other","path"], 1);
+		assertFileContains(localConfigPath, {
+			test: { some: { other: { path: 1 } } }
+		})
+	})
+})
+
+describe("setGlobal", () => {
+
+	beforeEach(() => {
+		mock({
+			// Just an empty dir for the global config
+			[globalConfigDir]: {}
+		});
+		conf.load();
+	});
+	afterEach(() => {
+		mock.restore();
+	});
+
+	it("should set a basic property", () => {
+		conf.setGlobal("test", 1);
+		assertFileContains(globalConfigPath, {
+			test: 1
+		})
+	})
+
+	it("should work with nested paths", () => {
+		conf.setGlobal("test.some.path", 1);
+		assertFileContains(globalConfigPath, {
+			test: { some: { path: 1 } }
+		})
+	})
+
+	it("should work with arrays", () => {
+		conf.setGlobal(["test","some","other","path"], 1);
+		assertFileContains(globalConfigPath, {
+			test: { some: { other: { path: 1 } } }
+		})
+	})
+})
+
+describe("get", () => {
+	beforeEach(() => {
+		mock({
+			// Dummy config files
+			[localConfigName]: JSON.stringify(dummyLocalConfig),
+			[globalConfigPath]: JSON.stringify(dummyGlobalConfig)
+		});
+		conf.load();
+	});
+	afterEach(() => {
+		mock.restore();
+	});
+
+	it("should retrieve local values", () => {
+		assert.equal("simple", conf.get("test"));
+	})
+	it("should retrieve global values", () => {
+		assert.equal("bagel", conf.get("testGlobal"));
+	})
+	it("should retrieve default values", () => {
+		assert.equal("potato", conf.get("testDefault"));
+	})
+
+	it("should retrieve a nested value", () => {
+		assert.equal("more complex path", conf.get("also.test.a"));
+	})
+	it("should also accept an array", () => {
+		assert.equal("more complex path", conf.get(["also","test","a"]));
+	})
+
+	it("should prefer local properties over global", () => {
+		assert.equal("local", conf.get("override"));
+	})
+	it("should prefer global properties over default", () => {
+		assert.equal("global", conf.get("override2"));
+	})
+
+})
+
 
 it("should work good :P", function() {
 	// TODO: indent properly (leaving for now to reduce git diff footprint)
